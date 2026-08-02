6ea2e3b2393644561640a31c65c1a90598d148da
OnlyRoohi <randiimusic22@gmail.com>
2026-08-02 12:02:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY  = getenv("HEROKU_API_KEY")
 
 # ─── Git / Upstream ──────────────────────────────────────────────────────────
-UPSTREAM_REPO   = getenv("UPSTREAM_REPO", "https://github.com/ragini19854-prog/hacker-x-madara")
+UPSTREAM_REPO   = getenv("UPSTREAM_REPO", "https://github.com/OnlyRoohi/Jtest")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN       = getenv("GIT_TOKEN", None)
 
